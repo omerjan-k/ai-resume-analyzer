b4a612ee4a1dd078c5f91616d93bcd08151eabc3
omerjan-k <omerjan.k@gmail.com>
2025-10-17 11:15:40 +0200

    Resume Feedback Page

--- app/routes.ts
+++ app/routes.ts
@@ -4,5 +4,6 @@ export default [
   index('routes/home.tsx'),
   route('auth', 'routes/auth.tsx'),
   route('upload', 'routes/upload.tsx'),
+  route('resume/:id', 'routes/resume.tsx'),
   route('*', 'routes/notFound.tsx'), // Catch-all 404 route
 ] satisfies RouteConfig;
